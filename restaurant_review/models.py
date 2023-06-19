3a531fdde145718a9d9438ac55202e60c3a0d146
Marcus Spaccavento <m_spaccavento@yahoo.com.au>
2023-06-19 14:16:12 +0800

    default for review_extra

--- restaurant_review/models.py
+++ restaurant_review/models.py
@@ -19,7 +19,7 @@ class Review(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     review_text = models.CharField(max_length=500)
     review_date = models.DateTimeField('review date')
-    review_extra = models.CharField(max_length=40)
+    review_extra = models.CharField(max_length=40, default='')
 
     def __str__(self):
         return f"{self.restaurant.name} ({self.review_date:%x})"
